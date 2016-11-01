a41ca86a2733a24478869a5e9a69323cd913c351
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-11-01 18:19:35 -0400

    ADD input to template

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,4 @@
 angular.module('bookInfo', [
+    'ngMaterial',
     'find'
 ]);
--- app/find/find.component.js
+++ app/find/find.component.js
@@ -2,6 +2,9 @@ angular.module('find').component('find', {
   templateUrl: 'find/find.template.html',
   controller: function FindController($scope) {
 
+    // Instantiate the book object
+    $scope.book = {};
+    $scope.book.name = "hello";
 
   }
 });
